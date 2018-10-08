0e427c31ea5ebb669d06816946ba1d19b12a71cf
Marc Siry <marc.siry.taipei@gmail.com>
2018-10-08 18:34:14 +0200

    First project revision

---
+++ app/src/main/java/com/lmb/fr/lmbapplicationnouga/LMB_Application.java
@@ -0,0 +1,13 @@
+package com.lmb.fr.lmbapplicationnouga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LMB_Application extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lmb__application);
+    }
+}
---
+++ app/src/main/java/com/lmb/fr/lmbapplicationnouga/LMB_SMS_Receiver.java
@@ -0,0 +1,87 @@
+package com.lmb.fr.lmbapplicationnouga;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class LMB_SMS_Receiver extends BroadcastReceiver {
+    private static final String TAG = "LmbSmsReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+
+        // Get the SMS message.
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+        String strMessageBody = "";
+        String format = null;
+        if (bundle != null) {
+            format = bundle.getString("format");
+        }
+        // Retrieve the SMS message received.
+        Object[] pdus = (Object[]) bundle.get("pdus");
+        if (pdus != null) {
+            // Check the Android version.
+            boolean isVersionM =
+                    (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+            // Fill the msgs array.
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                // Check Android version and use appropriate createFromPdu.
+                if (isVersionM) {
+                    // If Android version M or newer:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    // If Android version L or older:
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+
+                String numTel=msgs[i].getOriginatingAddress();
+
+                // Build the message to show.
+                strMessage += "SMS from " + numTel;
+                strMessageBody = msgs[i].getMessageBody();
+                strMessage += " :" + strMessageBody + "\n";
+
+                // Log and display the SMS message.
+                Log.d(TAG, "onReceive: " + strMessage);
+                Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
+
+                if(strMessageBody.toLowerCase().replaceAll("\\s", "").equals("ouvrir")) {
+                    // envoie d'un SMS de confirmation de l'ouverture du portail
+                    //PendingIntent pi = PendingIntent.getActivity(this, 0 , new Intent(this, sendmessage.class), 0);
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(numTel, null, "Ouverture du portail en cours. Si rien ne se passe, veillez ré-essayer dans 30 secondes.", null, null);
+                    //Toast.makeText(getApplicationContext(), "SMS sent.",
+
+
+                    //String num = "tel:" + numTel;
+                    String num = "tel:07000010009796";
+                    Intent appel = new Intent(Intent.ACTION_CALL, Uri.parse(num));
+                    //Toast.makeText(context, "le numero est:"+numTel,Toast.LENGTH_LONG).show();
+                    appel.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(appel);
+                }
+                else if(strMessageBody.toLowerCase().replaceAll("\\s", "").equals("test")) {
+                    // envoie d'un SMS de confirmation de l'ouverture du portail
+                    //PendingIntent pi = PendingIntent.getActivity(this, 0 , new Intent(this, sendmessage.class), 0);
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(numTel, null, "Test de l'application LMB", null, null);
+                    //Toast.makeText(getApplicationContext(), "SMS sent.",
+                }
+
+            }
+        }
+    }
+}
